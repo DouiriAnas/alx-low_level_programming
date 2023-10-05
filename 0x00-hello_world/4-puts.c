45508588c79fee9d910976339561ebe8881bde7e
DouiriAnas <douirianas.business@gmail.com>
2023-10-05 13:20:39 -0700

    update

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints a message with the puts function.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
